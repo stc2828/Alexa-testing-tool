0ac0b771bd891ae9669dfb46791f381103969a3c
knights207210 <xuha2442@colorado.edu>
2018-05-29 12:17:59 -0600

    openandstop

--- con.py
+++ con.py
@@ -3,6 +3,7 @@
 #Python 2.x program for Speech Recognition
 from gtts import gTTS
 import speech_recognition as sr
+import csv
  
 # This module is imported so that we can 
 # play the converted audio
@@ -30,46 +31,105 @@ for i, microphone_name in enumerate(mic_list):
 	print(microphone_name)
 	if microphone_name == mic_name:
 		device_id = i
+
+datas = ['name','question','response']
+
+with open('responses.csv', 'w', newline='') as f:
+	writer = csv.writer(f)
+	writer.writerow(datas)
  
-# The text that you want to convert to audio
-for j in ["WebMD"]:
-	mytext = 'Alexa, open ' + j
- 
-# Language in which you want to convert
-	language = 'en'
- 
-# Passing the text and language to the engine, 
-# here we have marked slow=False. Which tells 
-# the module that the converted audio should 
-# have a high speed
-	myobj = gTTS(text=mytext, lang=language, slow=False)
- 
-# Saving the converted audio in a mp3 file named
-# welcome 
-	myobj.save("1.mp3")
- 
-# Playing the converted file
-#first speak
-	print (mytext)
-	os.system("mpg321 1.mp3")
-#then listen
-	with sr.Microphone(device_index = device_id, sample_rate = sample_rate, 
-						chunk_size = chunk_size) as source:
-	#wait for a second to let the recognizer adjust the 
-	#energy threshold based on the surrounding noise level
-		r.adjust_for_ambient_noise(source)
-		print ("Say Something")
-	#listens for the user's input
-		audio = r.listen(source)
-		
-		try:
-			text = r.recognize_google(audio)
-			print("you said: " + text)
-	
-	#error occurs when google could not understand what was said
-	
-		except sr.UnknownValueError:
-			print("Google Speech Recognition could not understand audio")
-	
-		except sr.RequestError as e:
-			print("Could not request results from Google Speech Recognition service; {0}".format(e))
+    # The text that you want to convert to audio
+	for j in ["WebMD", "Mayo Clinic"]:
+
+	# "open"
+		row = []
+		row.append(j)
+		mytext = 'Alexa, open ' + j
+		row.append(mytext)
+    # Language in which you want to convert
+		language = 'en'
+     
+    # Passing the text and language to the engine, 
+    # here we have marked slow=False. Which tells 
+    # the module that the converted audio should 
+    # have a high speed
+		myobj = gTTS(text=mytext, lang=language, slow=False)
+     
+    # Saving the converted audio in a mp3 file named
+    # welcome 
+		myobj.save("open.mp3")
+     
+    # Playing the converted file
+    #first speak
+		print (mytext)
+		os.system("mpg321 open.mp3")
+    #then listen
+		with sr.Microphone(device_index = device_id, sample_rate = sample_rate, 
+    						chunk_size = chunk_size) as source:
+    	#wait for a second to let the recognizer adjust the 
+    	#energy threshold based on the surrounding noise level
+			r.adjust_for_ambient_noise(source)
+			print ("Alexa responses")
+    	#listens for the user's input
+			audio = r.listen(source)
+    		
+			try:
+				text = r.recognize_google(audio)
+				print("Alexa said: " + text)
+				row.append(text)
+    	
+    	#error occurs when google could not understand what was said
+    	
+			except sr.UnknownValueError:
+				print("Google Speech Recognition could not understand audio")
+    	
+			except sr.RequestError as e:
+				print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+			writer.writerow(row)
+	# "stop"
+		row_stop = []
+		row_stop.append(j)
+		mytext = 'Alexa, stop'
+		row_stop.append(mytext)
+    # Language in which you want to convert
+		language = 'en'
+     
+    # Passing the text and language to the engine, 
+    # here we have marked slow=False. Which tells 
+    # the module that the converted audio should 
+    # have a high speed
+		myobj = gTTS(text=mytext, lang=language, slow=False)
+     
+    # Saving the converted audio in a mp3 file named
+    # welcome 
+		myobj.save("stop.mp3")
+     
+    # Playing the converted file
+    #first speak
+		print (mytext)
+		os.system("mpg321 stop.mp3")
+    #then listen
+		with sr.Microphone(device_index = device_id, sample_rate = sample_rate, 
+    						chunk_size = chunk_size) as source:
+    	#wait for a second to let the recognizer adjust the 
+    	#energy threshold based on the surrounding noise level
+			r.adjust_for_ambient_noise(source)
+			print ("Alexa responses")
+    	#listens for the user's input
+			audio = r.listen(source)
+    		
+			try:
+				text = r.recognize_google(audio)
+				print("Alexa said: " + text)
+				row_stop.append(text)
+    	
+    	#error occurs when google could not understand what was said
+    	
+			except sr.UnknownValueError:
+				print("Google Speech Recognition could not understand audio")
+    	
+			except sr.RequestError as e:
+				print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+			writer.writerow(row_stop)
